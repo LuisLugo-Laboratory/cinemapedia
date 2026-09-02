d9a1cfef6985d54ed6f4fe86310243c8002d0bea
LuisLugo-Laboratory <luis2094lugo@gmail.com>
2026-09-02 15:18:22 -0600

    más comentarios

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,7 +3,8 @@ import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 //uso de go_router, se configura lo necesario para su uso. 
-//además de instalarlo en el puspect
+//además de instalarlo en el puspect..
+
 
 final appRrouter = GoRouter(
   initialLocation: '/',
